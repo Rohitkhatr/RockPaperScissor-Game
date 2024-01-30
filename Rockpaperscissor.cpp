1b69e7b2e930f9d88496001cfe2c17518885229b
Rohit Khatri <111869337+Rohitkhatr@users.noreply.github.com>
2024-01-30 18:09:23 +0530

    Update Rockpaperscissor.cpp

--- Rockpaperscissor.cpp
+++ Rockpaperscissor.cpp
@@ -4,33 +4,33 @@
 #include <stdlib.h>
 
 using namespace std;
-//                       Rock Paper Scissor Game
+//                                                          Rock Paper Scissor Game
 
 int main()
 {
     system("cls");
     system("Color 02");
-    cout << "\n\n\tWelcome To The -Rock Paper Scissor-\n";
-    cout << " \t 1. \tSTART\n";
-    cout << " \t 2. \tOUIT\n";
-
+    cout << "Welcome To The <ROCK PAPER SCISSORS>\n";
+    cout << " 1. START\n";
+    cout << " 2. OUIT\n";
+    cout << "-----------------------------------\n";
     char c;
     char c1;
     c = getch();
     if (c == '1')
     {
         string p1, p2;
-        cout << " \n \t\t\t\t Only Two Players Are Allowed At Same Time \n";
-        cout << "\n\t\t\t\t PLAYER ONE NAME - ";
+        cout << "RULE : Only Two Players Are Allowed At Same Time \n";
+        cout << "\nPLAYER ONE NAME - ";
         getline(cin, p1);
-        cout << "\t\t\t\t PLAYER TWO NAME - ";
+        cout << "PLAYER TWO NAME - ";
         getline(cin, p2);
-        cout << " \n\n\t\t\t\t-- Let's Start The Game -- \n\n";
-
+        cout << "\n-----------------------------------\n";
+        cout << "-- Let's Start The Game -- \n";
         char p;
         char pp;
-        cout << endl
-             << "\t\t" << p1 << "'s TURN "
+        cout << "\n"
+             << p1 << "'s TURN "
              << "( Choose 1-> Rock , 2-> Paper , 3-> Scissor) "
              << " : ";
     st:
@@ -38,8 +38,7 @@ int main()
         if (!(p >= 49 && p <= 51))
             goto st;
 
-        cout << endl
-             << "\t\t"
+        cout << "\n"
              << p2 << "'s TURN "
              << "( Choose 1-> Rock , 2-> Paper , 3-> Scissor) "
              << " : ";
@@ -48,34 +47,45 @@ int main()
         if (!(pp >= 49 && pp <= 51))
             goto ed;
 
-        // char  = "Rock";
-        // 2 = "Paper";
-        // 3 = "Scissor";
-
         cout << endl;
         if (p == '1' && pp == '1')
-            cout << "\n\t\t\a"
+        {
+            cout << "\n\a"
                  << "It's a Tie"
-                 << "\n\t\tBoth Choosed Rock";
+                 << "\nBoth Choosed Rock";
+            cout << "\n-----------------------------------\n";
+        }
         else if (p == '2' && pp == '2')
-            cout << "\n\t\t\a"
+        {
+            cout << "\n\a"
                  << "It's a Tie"
-                 << "\n\t\tBoth Choosed Paper";
+                 << "\nBoth Choosed Paper";
+            cout << "\n-----------------------------------\n";
+        }
         else if (p == '3' && pp == '3')
-            cout << "\n\t\t\a"
+        {
+            cout << "\n\a"
                  << "It's a Tie"
-                 << "\n\t\tBoth Choosed Scissor";
+                 << "\nBoth Choosed Scissor";
+            cout << "\n-----------------------------------\n";
+        }
         else if (p == '1' && pp == '3' || p == '2' && pp == '1' || p == '3' && pp == '2')
-            cout << "\n\t\t\a" << p1 << " Wins" << endl
-                 << "\t\t" << p1 << " Choosed " << p << " and " << p2 << " Chossed " << pp;
+        {
+            cout << "\n\a" << p1 << " Wins" << endl
+                 << p1 << " Choosed " << p << " and " << p2 << " Chossed " << pp;
+            cout << "\n-----------------------------------\n";
+        }
         else if (p == '3' && pp == '1' || p == '1' && pp == '2' || p == '2' && pp == '3')
-            cout << "\n\t\t\a" << p2 << " Wins" << endl
-                 << "\t\t" << p1 << " Choosed " << p << " and " << p2 << " Chossed " << pp;
+        {
+            cout << "\n\a" << p2 << " Wins" << endl
+                 << p1 << " Choosed " << p << " and " << p2 << " Chossed " << pp;
+            cout << "\n-----------------------------------\n";
+        }
     }
     else
         exit(0);
 
-    cout << "\n\n\t\tWant to Play Again -> Y/N ";
+    cout << "\nWant to Play Again -> Y/N ";
     char s;
     s = getch();
     if (s == 'Y' || s == 'y')
